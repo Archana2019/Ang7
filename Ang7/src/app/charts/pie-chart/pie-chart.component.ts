291f7ee0e4c9bbfbf09a5cbd27b8f4b159cd72d2
Archana2019 <u.archana@gmail.com>
2019-02-26 22:37:14 -0600

    added Pie chart

--- Ang7/src/app/charts/pie-chart/pie-chart.component.ts
+++ Ang7/src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+// import {  } from '../../shared/theme.colors' ;
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -8,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+  // @Input() inputData: any;
+  // @Input() limit: number;
+
+  pieChartData: number[] = [345,234,456];
+  pieChartLabels: string[] = ['ABCIndia','JaiBharath','DesiAmerican'];
+
+  colors: any[] = [
+    {
+      // backgroundColor: this.themeColors(theme),
+      backgroundColor:[] = ['#ffd16c','#f21f21','#098098'],
+      borderColor: 'goldenrod'
+
+    }
+  ];
+
+  pieChartType = 'doughnut';
 
   ngOnInit() {
-  }
+    // this.parseChartData(this.inputData, this.limit);
 
+  }
+  // parseChartData(res: any, limit?: number) {
+  //   const allData = res.slice(0, limit);
+  //   console.log(allData);
+  //   this.pieChartData = allData.map(x => _.values(x)[1]);
+  //   this.pieChartLabels = allData.map(x => _.values(x)[0]);
+  // }
+
+  // themeColors(setName: string): string[] {
+  //   const c = THEME_COLORS.slice(0)
+  //     .find(set => set.name === setName).colorSet;
+
+  //   return c;
+  // }
 }
